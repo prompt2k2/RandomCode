229fc85462e0dab64092526b7b7a98043a4a24e2
prompt2k2 <prompt2k2@gmail.com>
2022-10-17 13:09:30 +0100

    Loan v2

--- Lab3V1.py
+++ Lab3V1.py
@@ -32,83 +32,87 @@ Created on Fri Oct 14 12:42:28 2022
 # followed by percentages as above. Email a link to or a copy of the
 # source to the lab instructor before the assignment is due.
 #
+
 import matplotlib.pyplot as plt
 import numpy as np
 import math as math
-
 print('Version 1 - Solution')
 months = 0
-balance= 2000
-interestrate= 6.25/1200
+balance = 2000
+interestrate = 6.25/1200
 year = 0
 year = 0
-increaserate= 7/100
-artcost= 5550
-sciencecost= 6150
-engineeringcost= 6550
+increaserate = 7/100
+artcost = 5550
+sciencecost = 6150
+engineeringcost = 6550
 
-savings = [] #I added this variable to hold the new monthly savings
-for month in range(0,215):
+savings = []  # I added this variable to hold the new monthly savings
+for month in range(0, 215):
     month = month + 1
     year = year + 1/12
-    interest= (interestrate) * (balance)
+    interest = (interestrate) * (balance)
     balance = (balance + interest + 200)
-    
+
     balance2 = [balance]
     counter = [balance2]
     savings.append(round(balance, 2))
-    for year in range (0,18):
-           #It added the new value to the saving variable
-          counter = [balance2]
+    for year in range(0, 18):
+        # It added the new value to the saving variable
+        counter = [balance2]
 print(savings)
-print("The Total savings amount is $%.2f"%balance)
-for year in range(0,21):
-    year= year + 1
-    artincrease= artcost*increaserate
-    scienceincrease= sciencecost*increaserate
-    engineeringincrease= engineeringcost*increaserate
-    artcost= artcost+artincrease
-    sciencecost= sciencecost+scienceincrease
-    engineeringcost= engineeringcost+engineeringincrease
-    if year== 18:
-      artcost_19= artcost
-      sciencecost_19=sciencecost
-      engineeringcost_19=engineeringcost
-    if year== 19:
-      artcost_20= (artcost)
-      sciencecost_20=sciencecost
-      engineeringcost_20=engineeringcost
-    if year== 20:
-      artcost_21= (artcost)
-      sciencecost_21=sciencecost
-      engineeringcost_21=engineeringcost
-    if year== 21:
-      artcost_22= (artcost)
-      sciencecost_22=sciencecost
-      engineeringcost_22=engineeringcost
-totalartcost= artcost_19+artcost_20+artcost_21+artcost_22
-totalsciencecost =sciencecost_19+sciencecost_20+sciencecost_21+sciencecost_22
-totalengineeringcost= engineeringcost_19+engineeringcost_20+engineeringcost_21+engineeringcost_22
-print("The cost of the arts program is $%.2f" %totalartcost)
-print("The cost of the science program is $%.2f" %totalsciencecost)
-print("The cost of the engineering program is $%.2f" %totalengineeringcost)
+print("The Total savings amount is $%.2f" % balance)
+for year in range(0, 21):
+    year = year + 1
+    artincrease = artcost*increaserate
+    scienceincrease = sciencecost*increaserate
+    engineeringincrease = engineeringcost*increaserate
+    artcost = artcost+artincrease
+    sciencecost = sciencecost+scienceincrease
+    engineeringcost = engineeringcost+engineeringincrease
+    if year == 18:
+        artcost_19 = artcost
+        sciencecost_19 = sciencecost
+        engineeringcost_19 = engineeringcost
+    if year == 19:
+        artcost_20 = (artcost)
+        sciencecost_20 = sciencecost
+        engineeringcost_20 = engineeringcost
+    if year == 20:
+        artcost_21 = (artcost)
+        sciencecost_21 = sciencecost
+        engineeringcost_21 = engineeringcost
+    if year == 21:
+        artcost_22 = (artcost)
+        sciencecost_22 = sciencecost
+        engineeringcost_22 = engineeringcost
+totalartcost = artcost_19+artcost_20+artcost_21+artcost_22
+totalsciencecost = sciencecost_19+sciencecost_20+sciencecost_21+sciencecost_22
+totalengineeringcost = engineeringcost_19 + \
+    engineeringcost_20+engineeringcost_21+engineeringcost_22
+print("The cost of the arts program is $%.2f" % totalartcost)
+print("The cost of the science program is $%.2f" % totalsciencecost)
+print("The cost of the engineering program is $%.2f" % totalengineeringcost)
 # ------------Remove any code that is unnecessary--------------------------
 counter = [balance2]
-art= [totalartcost]
+art = [totalartcost]
 science = [totalsciencecost]
 engineering = [totalengineeringcost]
-for year in range(0,17):
+for year in range(0, 214):
     counter += [balance2]
     art += [totalartcost]
     science += [totalsciencecost]
     engineering += [totalengineeringcost]
-y=savings
-years = range(0,215)
-#y=balance2
-plt.plot( years,y,label = 'Savings Balance')
-plt.ylabel ('Amount $')
-plt.xlabel ('Month')
-plt.title ('Savings vs Tuition')
-plt.show()
+y = savings
+years = range(0, 215)
+# y=balance2
+plt.plot(years, y, label='Savings Balance')  # Plots the savings graph
 
-    
+plt.plot(years, art, label='Art Cost')  # imposes the art cost
+plt.plot(years, science, label='Science Cost')  # imposes the science cost
+# imposes the engineering cost
+plt.plot(years, engineering, label='Engineering Cost')
+plt.ylabel('Amount $')
+plt.xlabel('Month')
+plt.title('Savings vs Tuition')
+plt.show()
